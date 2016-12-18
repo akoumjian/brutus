66b2c30a91864047a3d55a6cdee4762205495850
Alec Koumjian <akoumjian@gmail.com>
2016-12-17 20:21:40 -0800

    this is such a dumb idea

--- README.rst
+++ README.rst
@@ -3,3 +3,20 @@ Brutus
 "When all you have is a hammer, every problem is a nail."
 
 Relentlessly hammer away at a machine until your software runs.
+
+Brutus will keep installing and configuring stuff until your command returns
+a success code. Brutus uses error messages.
+
+Installation
+------------
+
+..code-block::
+    pip install brutus
+
+
+Usage
+-----
+
+..code-block::
+    ## Use `sudo` as necessary
+    brutus COMMAND
--- __main__.py
+++ __main__.py
@@ -1,21 +1,73 @@
+from __future__ import print_function
 import six
 import argparse
-import subprocess
+import subprocess, shlex
 
 
-def install_salt():
-    """
-    Install the salt minion
-    """
-    from six.moves import urllib
-    import tempfile
-    response = urllib.request.urlopen('https://bootstrap.saltstack.com')
-    bootstrap_script = response.read()
-    handler, bootstrap_path = tempfile.mkstemp(suffix='.sh')
-    with open(bootstrap_path, 'wb') as bootstrap_file:
-        bootstrap_file.write(bootstrap_script)
+class Brutus():
 
-    subprocess.check_call(['sudo', 'sh', bootstrap_path])
+    def __init__(self, parser=None, **kwargs):
+        self.parser = parser
+        self.args = parser.parse_args()
+        self.command = self.args.command[0]
+        self.salt_client = self.load_salt_client()
+        self.return_val = None
+        self.error = None
+
+    def handle_error(self):
+        if self.error is None:
+            return
+
+        ## Detect error type and attempt to fix it
+
+    def run(self):
+        self.handle_error()
+        try:
+            self.return_val = self.run_command()
+            self.error = None
+        except subprocess.CalledProcessError as e:
+            self.error = e
+            self.run()
+
+    def run_command(self):
+        as_args = shlex.split(self.command)
+        return subprocess.check_output(
+            as_args,
+            stderr=subprocess.STDOUT
+        )
+
+    def __install_salt(self):
+        """
+        Install the salt minion
+        """
+        from six.moves import urllib
+        import tempfile
+        response = urllib.request.urlopen('https://bootstrap.saltstack.com')
+        bootstrap_script = response.read()
+        handler, bootstrap_path = tempfile.mkstemp(suffix='.sh')
+        with open(bootstrap_path, 'wb') as bootstrap_file:
+            bootstrap_file.write(bootstrap_script)
+
+        subprocess.check_call(['sudo', 'sh', bootstrap_path])
+
+    def load_salt_client(self):
+        ## Detect if Salt is installed, otherwise install salt
+        try:
+            import salt.client
+        except ImportError:
+            self.__install_salt()
+            import salt.client
+
+        ## Use local only config
+        mopts = salt.config.minion_config('')
+        mopts.update({
+            'file_client': 'local',
+            'master': 'localhost',
+        })
+
+        caller = salt.client.Caller(mopts=mopts)
+        return caller
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -23,29 +75,12 @@ def main():
     )
     parser.add_argument(
         'command',
-        metavar='COMMAND',
-        nargs='+',
+        nargs=1,
         help='Command that you want to run successfully.'
     )
 
-    args = parser.parse_args()
-
-    ## Detect if Salt is installed, otherwise install salt
-    try:
-        import salt.client
-    except ImportError:
-        install_salt()
-        import salt.client
-
-    mopts = salt.config.minion_config('')
-    mopts.update({
-        'file_client': 'local',
-        'master': 'localhost',
-    })
-
-    caller = salt.client.Caller(mopts=mopts)
-    output = caller.cmd('test.ping')
-    print(output)
+    brutus = Brutus(parser=parser)
+    brutus.run()
 
 
 if __name__ == "__main__":
